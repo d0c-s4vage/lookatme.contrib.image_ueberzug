02ab400c25f4b129c234ce7df24ba732558d86fc
James Johnson <d0c.s4vage@gmail.com>
2020-09-26 08:47:54 -0700

    Working with modifications to urwid_ueberzogen

--- lookatme/contrib/image_ueberzug.py
+++ lookatme/contrib/image_ueberzug.py
@@ -2,10 +2,11 @@
 An image renderer that uses ueberzug
 """
 
+import os
+import time
 import urwid
 import ueberzug.lib.v0 as ueberzug
 import urwid_ueberzogen as uw_uz
-import time
 
 
 from lookatme.exceptions import IgnoredByContrib
@@ -19,7 +20,24 @@ def root_urwid_widget(to_wrap):
 
 
 def image(link_uri, title, text):
-    placement = CANVAS.create_placement(time.time(), path="/home/saitama/Downloads/gitlab_logo.png")
-    blank_box = urwid.Pile([urwid.Text("") for x in range(15)])
+    link_uri = os.path.abspath(os.path.expanduser(link_uri))
+    if not os.path.exists(link_uri):
+        raise Exception("Local files only for images! (for now)")
+    placement = CANVAS.create_placement(
+        time.time(),
+        path=link_uri,
+        scaler=ueberzug.ScalerOption.FIT_CONTAIN.value,
+    )
+    try:
+        height = int(text)
+    except:
+        height = 30
+    blank_box = urwid.BoxAdapter(urwid.SolidFill(" "), height=height)
     img = uw_uz.Image(placement, blank_box)
     return [img]
+
+
+def shutdown():
+    if CANVAS is None:
+        return
+    CANVAS.__exit__()
